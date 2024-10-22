e0f28204dcf103a9688be04bedde703e6f0c8aa5
Vovchanchyk <oberigfast@gmail.com>
2024-10-22 21:05:42 +0300

    test: env secret

--- src/srp/srp.service.ts
+++ src/srp/srp.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
-
+const N =
+  '0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF';
 @Injectable()
 export class SrpService {
-  public readonly N = ethers.toBigInt(process.env.N);
-  public readonly g = ethers.toBigInt(process.env.g);
-  public readonly k = ethers.toBigInt(process.env.k);
+  public readonly N = ethers.toBigInt(N);
+  public readonly g = ethers.toBigInt(2);
+  public readonly k = ethers.toBigInt(3);
 
   generateServerCredentials(verifier: string) {
     const serverPrivateKey = ethers.toBigInt(ethers.randomBytes(32));
