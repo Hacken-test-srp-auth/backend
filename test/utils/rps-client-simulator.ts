5a9e66a28a55b2d6021fe3fd1c207af5f1400b5e
Vovchanchyk <oberigfast@gmail.com>
2024-10-22 17:20:58 +0300

    fix: test N 7

--- test/utils/rps-client-simulator.ts
+++ test/utils/rps-client-simulator.ts
@@ -1,9 +1,12 @@
 import { ethers } from 'ethers';
 
+const N =
+  '0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF';
+
 class SRPClientSimulator {
-  private N = ethers.toBigInt(process.env.N);
-  private g = ethers.toBigInt(process.env.g);
-  private k = ethers.toBigInt(process.env.k);
+  private N = ethers.toBigInt(N);
+  private g = ethers.toBigInt(2);
+  private k = ethers.toBigInt(3);
 
   private modPow(base: bigint, exponent: bigint, modulus: bigint): bigint {
     let result = 1n;
