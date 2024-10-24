875d1667cee79d0bad5c5f8d4c20f5eda1a3920f
Vovchanchyk <oberigfast@gmail.com>
2024-10-24 14:25:26 +0300

    test: log-cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-
+  console.log('process.env.CLIENT_URL =====>', process.env.CLIENT_URL);
   app.enableCors({
     exposedHeaders: ['Content-Disposition'],
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL],
     credentials: true,
   });
 
